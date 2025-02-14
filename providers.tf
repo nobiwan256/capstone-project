9bf4180cd355b69017bc19a5fea7c1b379adb504
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-14 16:49:02 +0100

    providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
+        source = "hashicorp/aws"
+        version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-  region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  token      = var.AWS_SESSION_TOKEN
+  region = "us-west-2"
 }
